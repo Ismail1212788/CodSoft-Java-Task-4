673d25df676097552dbf0761e4588845e9fdf790
Muhammad Ismail <muhammadismaillashari0@gmail.com>
2023-09-25 01:39:37 -0700

    initial commit

---
+++ src/main/java/com/mycompany/quizapplication/QuizApplication.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quizapplication;
+
+
+/**
+ *
+ * @author ali
+ */
+public class QuizApplication {
+
+    public static void main(String[] args) {
+  Login a=new Login();
+        a.show();
+    }
+}
